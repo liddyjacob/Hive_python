7d110d2e341f7c3a739fb65aa6ee1bf5779171ea
Jacob Liddy <liddyjacob@gmail.com>
2018-01-23 19:59:49 -0500

    Outlines what a hive is

---
+++ hive.py
@@ -0,0 +1,10 @@
+# hive.py
+# The hive is the structure which is formed
+# when playing the game of hive. It is a set
+# of pieces, on a honeycomb lattice.
+
+
+class Hive:
+    
+    def __init__(self):
+        self.pawns
