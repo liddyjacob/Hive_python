f927081ce28c92800a7fed760b547fcc7efad59a
Jacob Liddy <liddyjacob@gmail.com>
2018-01-31 01:20:11 -0500

    Added some descriptions of rules to be impemented

--- pawnutils.py
+++ pawnutils.py
@@ -27,3 +27,56 @@ def movement_rules(rules):
         return True
 
     return legalmove
+
+
+""" 
+## The slide rule ##
+
+A piece that follows the slide rule must be able to physically
+slide into its new position. Here is an example:
+x,y - unoccupied locations
+B - Black Player pawn
+W - White player pawn
+A - an ant pawn.
+
+. W B . .
+ B x . .
+. W W . .
+ B y . .
+. B(A). .
+
+The ant A can't move to x, as x does not have a nearby pair
+of empty spaces, two dots. This is an easy way to check for thae
+e slide rule. Note that here is another x that violates the slide
+rule yet has two empty spaces next to it:
+
+. . . . . . 
+ . . p p . .
+. p p . p . 
+ p x . >!< .
+. p . . p .
+ . p p p . .
+. A . . . .
+
+The only possible path brings us through the hole denoted by an
+explination point.
+
+"""
+
+def slide_rule(hive, init, final):
+    return NotImplemented
+
+
+"""
+    The hop rule means the piece can be on top of the hive.
+"""
+def hop_rule(hive, init, final):
+
+
+
+
+def jump_rule(hive, init, final):
+
+
+
+def length_rule
