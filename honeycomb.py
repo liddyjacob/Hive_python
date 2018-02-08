2a6b4cce20785e5f9cc53f762122fc2a9c5d2aa3
Jacob Liddy <liddyjacob@gmail.com>
2018-02-08 11:33:51 -0500

    Closer to PEP-8 Standards

--- honeycomb.py
+++ honeycomb.py
@@ -14,14 +14,15 @@ aralellogram lattice point
 
 ^
 |
-. . . . 
+. . . .
 |. . .
 . . . X
-|. . . 
-.-1-2-3-----> h 
+|. . .
+.-1-2-3-----> h
 
  X = (4,2)
 """
+
 
 class HCPoint:
 
@@ -32,7 +33,7 @@ class HCPoint:
     def __add__(self, other):
         h = self.h + other.h
         k = self.k + other.k
-        return HCPoint(h,k)
+        return HCPoint(h, k)
 
     def __eq__(self, other):
         return (self.h == other.h) and (self.k == other.k)
@@ -43,29 +44,31 @@ class HCPoint:
     def __hash__(self):
         return 3*self.h + 13*self.k
 
-#Converts a honeycomb point to euclidian
+
 def hpc_to_eucl(hcp):
+    """ Converts a honeycomb point to euclidian"""
 
     # Describes (0,1) from HoneyComb to euclidian,
     # "k" vector.
-    v_k = (0.0,1.0)
+    v_k = (0.0, 1.0)
 
     # Describes (1,0) from HoneyComb to euclidian
     # "h" vector.
-    v_k = (sqrt(3)/2.0,1.0/2.0)
+    v_h = (sqrt(3)/2.0, 1.0/2.0)
 
     xy_point = v_h * hpc.h + v_k * hpc.k
 
     return xy_point
 
-# Find the points within one euclidian unit of distance.
-# Returns the set of points
+
 def hpc_within_one(hpc):
-    
-    directions = [HCPoint(1,0), HCPoint(0,1),
-                  HCPoint(-1,1), HCPoint(-1,0),
-                  HCPoint(0,-1), HCPoint(1, -1)]
-    
+    """ Find the points within one euclidian unit of distance.
+        Returns the set of points
+    """
+
+    directions = [HCPoint(1, 0), HCPoint(0, 1),
+                  HCPoint(-1, 1), HCPoint(-1, 0),
+                  HCPoint(0, -1), HCPoint(1, -1)]
     nearby_points = []
 
     for direction in directions:
